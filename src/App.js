5f1ca32f4d4a728ba4123f609714d4cea939d2b9
Vasilka Stefanova <vasilka.stefanova.a32@learn.telerikacademy.com>
2021-10-23 18:31:41 +0300

    project set-up

---
+++ src/App.js
@@ -0,0 +1,14 @@
+/* eslint-disable require-jsdoc */
+import './App.css';
+import React from 'react';
+
+// eslint-disable-next-line func-style
+function App() {
+  return (
+    <div className="App">
+      Library Project
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+ReactDOM.render(
+  // eslint-disable-next-line indent
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
+
