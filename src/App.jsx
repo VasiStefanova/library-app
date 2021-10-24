67454026e5e17c7052d3e542e581fe0f2dad3902
Vasilka Stefanova <vasilka.stefanova.a32@learn.telerikacademy.com>
2021-10-24 18:52:38 +0300

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,11 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavigationBar from './Navigation/Navbar.jsx';
 
+
 // eslint-disable-next-line func-style
 function App() {
   return (
-    <>
-      <div className="App">
-        Library Project
-      </div>
-      <NavigationBar />
-    </>
+    <NavigationBar />
   );
 }
 
--- src/Navigation/Navbar.jsx
+++ src/Navigation/Navbar.jsx
@@ -3,16 +3,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
 
 const NavigationBar = () => {
   return (
     <Navbar bg="dark" variant="dark" className="navbar navbar-default">
-      <Container>
-        <Navbar.Brand href="#home">HOME</Navbar.Brand>
+      <Container fluid>
+        <Navbar.Brand as={Link} to="/home">HOME</Navbar.Brand>
         <Nav className="me-auto">
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#register">Register</Nav.Link>
-          <Nav.Link href="#login">Log In</Nav.Link>
+          <Nav.Link as={Link} to="/register">Register</Nav.Link>
+          <Nav.Link as={Link} to="/login">Log In</Nav.Link>
         </Nav>
       </Container>
     </Navbar>
