a0198811284518ad8f057606e2c2bfa868253673
Vasilka Stefanova <vasilka.stefanova.a32@learn.telerikacademy.com>
2021-10-25 12:10:45 +0300

    react routing moved to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,23 @@ import './App.css';
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavigationBar from './Navigation/Navbar.jsx';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './Components/Home/Home';
+import Register from './Components/Register/Register';
+import LogIn from './Components/LogIn/LogIn';
 
 
 // eslint-disable-next-line func-style
 function App() {
   return (
-    <NavigationBar />
+    <BrowserRouter>
+      <NavigationBar />
+      <Switch>
+        <Route path="/home" component={Home} />
+        <Route path="/register" component={Register} />
+        <Route path="/login" component={LogIn} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,20 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Home from './Components/Home/Home';
-import Register from './Components/Register/Register';
-import LogIn from './Components/LogIn/LogIn';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-    <Switch>
-      <Route path="/home" component={Home} />
-      <Route path="/register" component={Register} />
-      <Route path="/login" component={LogIn} />
-    </Switch>
-  </BrowserRouter>,
+  <App />,
   // eslint-disable-next-line no-undef
   document.getElementById('root')
 );
