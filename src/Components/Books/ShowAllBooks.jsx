bb46cc470286fc42c6a83031170f27d51c54ffbc
Vasilka Stefanova <vasilka.stefanova.a32@learn.telerikacademy.com>
2021-10-27 16:44:00 +0300

    #5 major component changes

--- src/Components/Books/ShowAllBooks.jsx
+++ src/Components/Books/ShowAllBooks.jsx
@@ -1,29 +1,46 @@
 /* eslint-disable react/jsx-indent-props */
 /* eslint-disable linebreak-style */
 import React from 'react';
-import { data } from '../../Data/data.js';
-import ViewIndividualBook from './ViewIndividualBook';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import { useState, useEffect } from 'react';
 
-const ShowAllBooks = () =>{
 
-  return data.map(book =>{
+const ShowAllBooks = ({ history }) =>{
+
+  const [books, setBooks] = useState([]);
+
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/api/v1/books/`, {
+      headers: {
+      // Hardcoded authorization token//
+      // eslint-disable-next-line max-len
+        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjIsInVzZXJuYW1lIjoibW9ycGhldXMiLCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE2MzQ1NzgwMzUsImV4cCI6MTY0MzIxODAzNX0.HWMHorMgf3a2EvNPVsvtKxJOFVedn-3kLOANnSiFzmk',
+      },
+    })
+
+      .then(response => response.json())
+      .then(data => setBooks(data.books))
+      .catch(err => console.error(err));
+  }, []);
+
+
+  const routeChange = (id) =>{
+    const path = `/books/${id}`;
+    history.push(path);
+  };
+  return books.map(book =>{
     return (
-      <div key={book.id}>
-        <img src={`http://localhost:5000/covers/${book.cover}`} />
-        <div>{book.title} - {book.author}</div>
-        <button onClick={() =>
-          <ViewIndividualBook
-            id={book.id}
-            title={book.title}
-            author={book.author}
-            status={book.status}
-            cover={book.cover}
-            rating={book.rating}
-          />}
-        >Details
-        </button>
-      </div>
-
+      <Container key={book.id}>
+        <div>
+          <img src={`http://localhost:5000/covers/${book.cover}`} />
+          <div>{book.title} - {book.author}</div>
+          <Button onClick={() => routeChange(book.id)}>
+            Details
+          </Button>
+        </div>
+      </Container>
     );
   });
 };
