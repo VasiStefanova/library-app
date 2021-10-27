3dca4e83a4f1c963087c7dbe61875061d3fca172
MaxKsantis <maxksanthis@gmail.com>
2021-10-27 15:04:03 +0300

    view cover of books

--- src/Components/Books/ShowAllBooks.jsx
+++ src/Components/Books/ShowAllBooks.jsx
@@ -1,17 +1,27 @@
+/* eslint-disable react/jsx-indent-props */
 /* eslint-disable linebreak-style */
 import React from 'react';
 import { data } from '../../Data/data.js';
-// import ViewIndividualBook from './ViewIndividualBook.jsx';
+import ViewIndividualBook from './ViewIndividualBook';
 
 const ShowAllBooks = () =>{
 
   return data.map(book =>{
-
     return (
       <div key={book.id}>
-        <img scr={book.cover} />
+        <img src={`http://localhost:5000/covers/${book.cover}`} />
         <div>{book.title} - {book.author}</div>
-        <button>Details</button>
+        <button onClick={() =>
+          <ViewIndividualBook
+            id={book.id}
+            title={book.title}
+            author={book.author}
+            status={book.status}
+            cover={book.cover}
+            rating={book.rating}
+          />}
+        >Details
+        </button>
       </div>
 
     );
--- src/Components/Books/ViewIndividualBook.jsx
+++ src/Components/Books/ViewIndividualBook.jsx
@@ -1,27 +1,26 @@
 import React from 'react';
-import { data } from '../../Data/data.js';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
-const ViewIndividualBook = () =>{
-  const foundBook = data.find(book => book.id === 3);
+const ViewIndividualBook = (props) =>{
+
   return (
-    <div id={foundBook.id} className='ViewIndividualBook'>
-      <img scr={foundBook.cover} />
-      <div>{foundBook.title} - {foundBook.author}</div>
-      <div>Status: {foundBook.status} | Rating: {foundBook.rating}</div>
+    <div id={props.id} className='ViewIndividualBook'>
+      <img src={`http://localhost:5000/covers/${props.cover}`} />
+      <div>{props.title} - {props.author}</div>
+      <div>Status: {props.status} | Rating: {props.rating}</div>
     </div>
   );
 
 };
 
-// ViewIndividualBook.propTypes = {
-//   id: PropTypes.string,
-//   title: PropTypes.string,
-//   author: PropTypes.string,
-//   status: PropTypes.string,
-//   cover: PropTypes.string,
-//   rating: PropTypes.number
+ViewIndividualBook.propTypes = {
+  id: PropTypes.number,
+  title: PropTypes.string,
+  author: PropTypes.string,
+  status: PropTypes.string,
+  cover: PropTypes.string,
+  rating: PropTypes.number
 
-// };
+};
 
 export default ViewIndividualBook;
