1ed6cace16ad3b9a0b0c1bccc0d59db415549d16
Vasilka Stefanova <vasilka.stefanova.a32@learn.telerikacademy.com>
2021-10-28 22:41:25 +0300

    react context  and token & user retrieval

---
+++ src/context/auth-context.js
@@ -0,0 +1,24 @@
+import { createContext } from 'react';
+import jwt from 'jsonwebtoken';
+
+export const AuthContext = createContext({
+  user: {
+    id: 0,
+    username: '',
+  },
+  isLoggedIn: false,
+  setAuth() { },
+});
+
+export const getToken = () => localStorage.getItem('token') || '';
+
+export const getUser = () => {
+  console.log('getUser called');
+  try {
+    const tokenFromStorage = getToken();
+    // TODO: Check if token is valid;
+    return jwt.decode(tokenFromStorage);
+  } catch {
+    return null;
+  }
+};
