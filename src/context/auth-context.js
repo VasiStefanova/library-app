e50c25e2f6055a33f0df57cc069591f92f743ae8
Vasilka Stefanova <vasilka.stefanova.a32@learn.telerikacademy.com>
2021-10-29 20:14:46 +0300

    console.log in comments

--- src/context/auth-context.js
+++ src/context/auth-context.js
@@ -13,9 +13,10 @@ export const AuthContext = createContext({
 export const getToken = () => localStorage.getItem('token') || '';
 
 export const getUser = () => {
-  console.log('getUser called');
+  // console.log('getUser called');
   try {
     const tokenFromStorage = getToken();
+    // console.log(jwt.decode(tokenFromStorage));
     // TODO: Check if token is valid;
     return jwt.decode(tokenFromStorage);
   } catch {
