59e5c3de696b015d05411904c31e3ab58a6443c2
Vasilka Stefanova <vasilka.stefanova.a32@learn.telerikacademy.com>
2021-11-03 22:51:02 +0200

    #11 prop types updated

--- src/Components/Reviews/Book Reviews/User Review/UpdateUserReviewsLink.jsx
+++ src/Components/Reviews/Book Reviews/User Review/UpdateUserReviewsLink.jsx
@@ -1,25 +1,26 @@
 
 import React, { useContext } from 'react';
-import { propTypes } from 'react-bootstrap/esm/Image';
+import PropTypes from 'prop-types';
 import { useHistory } from 'react-router-dom';
 import { AuthContext } from '../../../../context/auth-context';
 import './UpdateUserReviewsLink.css';
 
-const UpdateUserReviewsLink = ({ userId }) => {
+const UpdateUserReviewsLink = ({ userId, bookId }) => {
   const { user } = useContext(AuthContext);
-  const history = useHistory;
+  const history = useHistory();
 
 
   return user.sub === userId ?
     <a
-      onClick={() => history.push('/book/:id/update-review')}
-      className="btn btn-link disabled update-reviews-link"
+      onClick={() => history.push(`/books/${bookId}/update-review`)}
+      className="btn btn-link update-reviews-link"
     > Update your review
     </a> :
     null;
 };
 
 UpdateUserReviewsLink.propTypes = {
-  userId: propTypes.number
+  userId: PropTypes.number,
+  bookId: PropTypes.number
 };
 export default UpdateUserReviewsLink;
