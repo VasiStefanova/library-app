c8eedf353437a34072664168440214165d0edc63
Vasilka Stefanova <vasilka.stefanova.a32@learn.telerikacademy.com>
2021-11-08 23:54:25 +0200

    #9 refactored as part of ViewIndividualBook

--- src/Components/Reviews/Book Reviews/ReadBookReviews.jsx
+++ src/Components/Reviews/Book Reviews/ReadBookReviews.jsx
@@ -1,25 +1,28 @@
 import Container from 'react-bootstrap/Container';
-import Button from 'react-bootstrap/Button';
+import PropTypes from 'prop-types';
 import Row from 'react-bootstrap/Row';
 import React from 'react';
-import { useEffect, useState } from 'react';
-import Figure from 'react-bootstrap/Figure';
+import { useEffect, useState, useContext } from 'react';
 import './ReadBookReviews.css';
-import UserReview from './User Review/UserReviwes';
-import PropTypes from 'prop-types';
+import UserReview from './User Review/UserReviews';
+import { getToken } from '../../../context/auth-context';
+import LeaveReview from './User Review/LeaveReview';
+import { AuthContext } from '../../../context/auth-context';
 
 
-const ReadBookReviews = ({ match }) => {
-  const id = match.params.id;
+const ReadBookReviews = ({ id }) => {
+  // const id = props.match.params.id;
 
   const [bookInfo, setBookInfo] = useState({});
   const [reviews, setReviews] = useState([]);
+  // This will be used by child components
+  // to trigger useEffect on some change in the child.
+  const [reRender, setReRender] = useState({});
+  const { user } = useContext(AuthContext);
   useEffect(() => {
     fetch(`http://localhost:5000/api/v1/books/${id}/reviews`, {
       headers: {
-        // Hardcoded authorization token//
-        // eslint-disable-next-line max-len
-        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjIsInVzZXJuYW1lIjoibW9ycGhldXMiLCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE2MzQ1NzgwMzUsImV4cCI6MTY0MzIxODAzNX0.HWMHorMgf3a2EvNPVsvtKxJOFVedn-3kLOANnSiFzmk',
+        'Authorization': `Bearer ${getToken()}`,
       },
     })
       .then(response => response.json())
@@ -28,36 +31,26 @@ const ReadBookReviews = ({ match }) => {
         setBookInfo(data[0] || {});
       })
       .catch(err => console.error(err));
-  }, [id]);
+  }, [id, reRender]);
+
 
   return (
     <>
       {reviews.length ?
         <>
-          <Container className='book-container'>
-            <Figure>
-              <Figure.Image
-                thumbnail
-                width={171}
-                height={180}
-                alt={bookInfo.bookTitle}
-                src={`http://localhost:5000/covers/${bookInfo.cover}`}
-              />
-              <Figure.Caption>
-                Book Title: {bookInfo.bookTitle}
-              </Figure.Caption>
-            </Figure>
-          </Container>
-
           <Container className="heading">
             <h2> User Reviews: </h2>
-          </Container>
+            {reviews.find(review => review.userId === user.sub) ?
+              null:
+              <LeaveReview id={id} />}
+          </Container>s
 
           <Container>
             <Row className="reviews-container">
               {reviews.map(review => <UserReview
                 key={review.userId}
                 bookId={+id}
+                setReRender={setReRender}
                 title={review.title}
                 content={review.content}
                 userId={review.userId}
@@ -68,13 +61,13 @@ const ReadBookReviews = ({ match }) => {
         </> :
         <Container className="no-reviews-msg align-self-center">
           <h3> There are no user reviews for this book yet! Be the first to write one.</h3>
-          <Button variant="outline-dark">Back to Book</Button>
+          <LeaveReview id={id} />
         </Container>}
     </>
   );
 };
 
 ReadBookReviews.propTypes = {
-  match: PropTypes.object,
+  id: PropTypes.string,
 };
 export default ReadBookReviews;
