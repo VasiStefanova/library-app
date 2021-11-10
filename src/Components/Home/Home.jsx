b89433dd8394e39155b68a52814db5dfa2bee7bd
Vasilka Stefanova <vasilka.stefanova.a32@learn.telerikacademy.com>
2021-11-10 22:51:49 +0200

    #1 alert message added for banned users

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -4,24 +4,32 @@ import './Home.css';
 import Container from 'react-bootstrap/Container';
 import Carousel from 'react-bootstrap/Carousel';
 import Card from 'react-bootstrap/Card';
+import AlertDismissible from '../Alerts/ErrorAlert';
 
 // import Row from 'react-bootstrap/Row';
 // import Col from 'react-bootstrap/Col';
 
 const Home = () => {
   const [books, setBooks] = useState([]);
-  const { user, isLoggedIn } = useContext(AuthContext);
+  const { user, isLoggedIn, isBanned } = useContext(AuthContext);
+  const [errMsg, setErrMsg] = useState('');
+
   useEffect(() => {
     fetch(`http://localhost:5000/api/v1/books/`, {
       headers: {
-      // eslint-disable-next-line max-len
-        'Authorization': `Bearer ${getToken()}`,
+      // hard-coded token to show carousel//
+        // eslint-disable-next-line max-len
+        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjEsInVzZXJuYW1lIjoibmVvIiwicm9sZSI6ImFkbWluIiwiaWF0IjoxNjM0NTc4MDEwLCJleHAiOjE2NDMyMTgwMTB9.Pt1dfv0OcyfThoF5hCbeFVq9KX4rN4QNL1EqlMd3-UI',
       },
     })
 
       .then(response => response.json())
       .then(data => setBooks(data.books))
-      .catch(err => console.error(err));
+      .catch(err => {
+        setErrMsg(err.message);
+        console.error(err);
+      }
+      );
   }, []);
 
   const carouselBooks = books.map((book) => {
@@ -41,6 +49,7 @@ const Home = () => {
     <Container fluid className="welcome-msg">
       <h1>Welcome to Library Project!</h1>
       {isLoggedIn ? <h4>Hello {user.username}!</h4> : <h4>Please log in!</h4>}
+      {isLoggedIn && isBanned ? <AlertDismissible isBanned={isBanned} /> : null}
       <Carousel>{carouselBooks}</Carousel>
     </Container>
   );
