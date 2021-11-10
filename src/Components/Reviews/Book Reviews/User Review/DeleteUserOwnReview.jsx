6b41cdf626e89984bbf5b09efb69ecdb81a1336a
Vasilka Stefanova <vasilka.stefanova.a32@learn.telerikacademy.com>
2021-11-10 22:58:16 +0200

    #12 disabled functionalities for banned users

--- src/Components/Reviews/Book Reviews/User Review/DeleteUserOwnReview.jsx
+++ src/Components/Reviews/Book Reviews/User Review/DeleteUserOwnReview.jsx
@@ -6,7 +6,7 @@ import { getToken } from '../../../../context/auth-context';
 
 const DeleteUserOwnReview = ({ userId, bookId, setReRender }) => {
 
-  const { user } = useContext(AuthContext);
+  const { user, isBanned } = useContext(AuthContext);
 
   const deleteReview = () => {
     fetch(`http://localhost:5000/api/v1/books/${bookId}/reviews`, {
@@ -22,8 +22,8 @@ const DeleteUserOwnReview = ({ userId, bookId, setReRender }) => {
       .catch(err => console.error(err));
   };
 
-  return user.sub === userId ?(
-    <Button variant="outline-danger" size="sm" onClick={deleteReview}>
+  return user.id === userId ?(
+    <Button disabled={isBanned} variant="outline-danger" size="sm" onClick={deleteReview}>
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-x" viewBox="0 0 16 16">
         <path d="M4.646 4.646a.5.5 0 0 1 .708
          0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5
