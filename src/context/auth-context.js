8803a98734095a2337a365ab18e767bf6d065c0f
Vasilka Stefanova <vasilka.stefanova.a32@learn.telerikacademy.com>
2021-11-10 23:02:07 +0200

    authenthication from the server added

--- src/context/auth-context.js
+++ src/context/auth-context.js
@@ -8,17 +8,55 @@ export const AuthContext = createContext({
     role: ''
   },
   isLoggedIn: false,
+  isBanned: false,
   setAuth() { },
 });
 
 export const getToken = () => localStorage.getItem('token') || '';
 
-export const getUser = () => {
-  try {
-    const tokenFromStorage = getToken();
-    // TODO: Check if token is valid;
-    return jwt.decode(tokenFromStorage);
-  } catch {
-    return null;
+let userInfo = null;
+
+export const getDecodedUser = () => {
+  let result = null;
+  const tokenFromStorage = getToken();
+  if (tokenFromStorage) {
+    result = jwt.decode(tokenFromStorage);
   }
+
+  return result;
 };
+
+export const clearUser = () => {
+  userInfo = null;
+};
+
+export const getUser = async () => {
+  if (!userInfo) {
+    try {
+      const decodedUser = getDecodedUser();
+      if (decodedUser) {
+        const user = await fetchUserInfo(decodedUser.sub);
+        userInfo = {
+          user,
+          isLoggedIn: true,
+          isBanned: user.banstatus?.banned
+        };
+      }
+    } catch {
+      userInfo = null;
+    }
+  }
+
+  return userInfo;
+};
+
+export const fetchUserInfo = (userId) => {
+  return fetch(`http://localhost:5000/api/v1/users/${userId}`, {
+    headers: {
+      'Authorization': `Bearer ${getToken()}`,
+    }
+  })
+    .then(response => response.json())
+    .catch(err => console.error(err));
+};
+
