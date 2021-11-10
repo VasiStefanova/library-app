2718854513cb86ba37f5278b77d8f46bfb61b722
Vasilka Stefanova <vasilka.stefanova.a32@learn.telerikacademy.com>
2021-11-10 23:02:46 +0200

    users link added for admins to manage

--- src/Navigation/Navbar.jsx
+++ src/Navigation/Navbar.jsx
@@ -4,13 +4,14 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
-import { AuthContext } from '../context/auth-context';
+import { AuthContext, clearUser } from '../context/auth-context';
 
 const NavigationBar = () => {
   const { user, isLoggedIn, setAuth } = useContext(AuthContext);
 
   const logout = () => {
     localStorage.removeItem('token');
+    clearUser();
     setAuth({
       user: { role: '' },
       token: '',
@@ -23,15 +24,15 @@ const NavigationBar = () => {
       <Container fluid>
         <Navbar.Brand as={Link} to="/home">HOME</Navbar.Brand>
         <Nav className="me">
-          {isLoggedIn === false ?
+          {isLoggedIn ?
+            <>
+              {user?.role === 'admin' && <Nav.Link as={Link} to="/users/admin">Users</Nav.Link>}
+              <Nav.Link as={Link} to="/books">Books</Nav.Link>
+              <Nav.Link onClick={logout}>Logout</Nav.Link>
+            </>:
             <>
               <Nav.Link as={Link} to="/login">Log In</Nav.Link>
               <Nav.Link as={Link} to="/register">Register</Nav.Link>
-            </> :
-            <>
-              {user.role === 'admin' && <Nav.Link as={Link} to="/users/admin">Users</Nav.Link>}
-              <Nav.Link as={Link} to="/books">Books</Nav.Link>
-              <Nav.Link onClick={logout}>Logout</Nav.Link>
             </>}
         </Nav>
       </Container>
